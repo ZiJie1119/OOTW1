f51a9a8d4e228598ff149c69fc74252db5750b05
fan9704 <cxz123499@gmail.com>
2022-10-14 20:37:42 +0800

    Operating System Detection

--- OOTW1/src/bridge/Main.java
+++ OOTW1/src/bridge/Main.java
@@ -4,13 +4,21 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        WindowImpl windowsImpl;
+        AbstractWindow window;
 
-        WindowImpl windowsImpl = new WindowsImpl();
-        WindowImpl xWindowImpl = new XWindowImpl();
+        WindowImpl xWindowImpl ;
+        System.out.println("OS Name:" +System.getProperty("os.name") );
+        System.out.println("OS Version:" + System.getProperty("os.version") );
+        System.out.println("OS Architecture:" + System.getProperty("os.arch") );
 
-        AbstractWindow window = new Window(xWindowImpl);
-
-//        AbstractWindow window = new Window(windowsImpl);
+        if(System.getProperty("os.name").equals("Windows 10")){
+            windowsImpl = new WindowsImpl();
+            window = new Window(windowsImpl);
+        }else{
+            xWindowImpl= new XWindowImpl();
+            window = new Window(xWindowImpl);
+        }
 
         window.DrawRect();
         window.DrawText();
